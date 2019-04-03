45d2799c4e75f2bf18e73b699d38d7386308796b
pbs <1768804958@qq.com>
2019-04-03 21:32:31 +0800

    0403

--- 0403/新建1.py
+++ 0403/新建1.py
@@ -1,6 +1,7 @@
 
 def getinfo(self):
     print('getinfo++')
+    # return '父方法'
 g = type('Goods',(),{'id':'Gooodsid','name':'pbs','getinfo':getinfo})
 
 def getname(self):
@@ -10,15 +11,16 @@ f = type('Food',(g,),{'type':'qwe','getname':getname})
 g1 = g()
 print(g1.id,g1.name)
 g1.getinfo()
+# print(g1.getinfo())
 f1 = f()
 print(f1.id,f1.name,f1.type)
 f1.getname()
 f1.getinfo()
 
 
-g().getinfo()
-print(g.id,g.name)
-
-f().getinfo()
-f().getname()
-print(f.id,f.name,f.type)
+# g().getinfo()
+# print(g.id,g.name)
+#
+# f().getinfo()
+# f().getname()
+# print(f.id,f.name,f.type)
