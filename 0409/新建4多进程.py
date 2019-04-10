cc7dfd4ba97cb405b657107e3ede682cc689af0d
pbs <1768804958@qq.com>
2019-04-10 11:44:05 +0800

    0409

--- 0409/新建4多进程.py
+++ 0409/新建4多进程.py
@@ -1,5 +1,6 @@
 from multiprocessing import Pool
-import requests,re
+import requests,re,time
+
 
 def write(i,d):
     response = requests.get(i)
@@ -9,9 +10,18 @@ def write(i,d):
     with open(k, 'wb') as f:
         f.write(content)
 
+def timecount(f):
+    ''' 21.395724296569824 '''
+    def fun():
+        start = time.time()
+        f()
+        end = time.time()
+        print(end - start)
+    return  fun
 
+@timecount
 def main():
-    pool = Pool(5)
+    pool = Pool(4)
     d = 0
     with open('1.txt','r') as f:
         s = f.read()
