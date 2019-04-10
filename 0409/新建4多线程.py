9ef2df9b162945e41584ed05f22581bdadbc093e
pbs <1768804958@qq.com>
2019-04-10 11:43:56 +0800

    0409

--- 0409/新建4多线程.py
+++ 0409/新建4多线程.py
@@ -1,4 +1,5 @@
 import threading,re,requests
+import time
 
 def fun(i,d):
     response = requests.get(i)
@@ -8,12 +9,30 @@ def fun(i,d):
     with open(k, 'wb') as f:
         f.write(content)
 
-if __name__ == '__main__':
+def timecount(f):
+    '''20.84735417366028'''
+    def fun():
+        start = time.time()
+        f()
+        end = time.time()
+        print(end - start)
+    return  fun
+
+@timecount
+def main():
     d = 0
-    with open('1.txt','r') as f:
+    with open('1.txt', 'r') as f:
         s = f.read()
         result = re.findall(r'(.*?)\n', s, re.S)
+        theradlist = []
         for i in result:
-            t = threading.Thread(target=fun,args=(i,d))
+            # d1 = re.split('/',i)[-1]
+            t = threading.Thread(target=fun, args=(i, d))
             t.start()
-            d = d+1
+            d = d + 1
+            theradlist.append(t)
+        for i in theradlist:
+            i.join()
+
+if __name__ == '__main__':
+   main()
